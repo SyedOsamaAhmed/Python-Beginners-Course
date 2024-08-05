2fcab48773d2a8ae1f15388326fbfd9d97dd7c86
Syed Osama Ahmed <batch17.026@gmail.com>
2024-08-05 12:40:09 +0500

    covered variables,string methods,string formating, input method, difference b/w replace() and in python

---
+++ app.py
@@ -0,0 +1,45 @@
+
+
+price = 10
+rating = 4.9
+is_published = True
+
+
+name = 'John Smith'
+age = 20
+is_new = True
+
+
+# name = input("What is your name? ")
+# favourite_color = input("What is your favourite color? ")
+# print(name+' likes '+favourite_color)
+
+# birth_year = input("Birth Year: ")
+# age = 2024-int(birth_year)
+# print(type(birth_year))
+# print(type(age))
+
+# weight_in_lb = input("Enter your weight: ")
+# weight_in_kg = int(weight_in_lb)*0.45
+# print(weight_in_kg)
+# course = '''Hi Osama
+
+# This is the first email.
+
+# Thank you
+# support team
+
+# '''
+
+course = "Python for beginners"
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('o'))
+print(course.replace('beginners', 'absolute beginners'))
+print('Python' in course)
+
+# first_name = "John"
+# last_name = "Smith"
+# message = f'{first_name} [{last_name}] is a coder.'
+# print(message)
